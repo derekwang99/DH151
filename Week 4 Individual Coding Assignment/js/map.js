f46f4f5b991cf64cc485c3909248d434e1421c29
derekwang99 <57509494+derekwang99@users.noreply.github.com>
2021-04-25 17:27:13 -0700

    4/25/21

--- Week 4 Individual Coding Assignment/js/map.js
+++ Week 4 Individual Coding Assignment/js/map.js
@@ -1,11 +1,19 @@
 // Global variables
-let map;
+var map;
 let lat = 0;
 let lon = 0;
 let zl = 3;
-let path = "data/plastic_pollution.csv";
+var path = "data/plastic_pollution.csv";
 let markers = L.featureGroup();
-let time = ['2008','2009','2010','2011','2012','2013']
+let time2007 = ['2007'];
+let time2008 = ['2008'];
+let time2009 = ['2009'];
+let time2010 = ['2010'];
+let time2011 = ['2011'];
+let time2012 = ['2012'];
+let time2013 = ['2013'];
+
+let clear =  ['Reset'];
 
 // initialize
 $( document ).ready(function() {
@@ -36,15 +44,106 @@ function readCSV(path){
 		}
 	});
 }
-
-time.forEach(function(item){
-	$('.sidebar').append(`<div class = "sidebar-item" > ${item} </div>`)
+// Reset Map
+clear.forEach(function(item){
+	$('.sidebar').append(`<div class = "sidebar-item" onclick = "resetMap()"> Clear Map  </div>`)
 });
 
-	//$('.sidebar').append(`<div class = "sidebar-item" onclick = "flyToIndex(${index})">${item.title} </div>`)
 
 
 
+function resetMap(){
+	markers.clearLayers();
+}
+
+
+
+// 2007
+time2007.forEach(function(item){
+	$('.sidebar').append(`<div class = "sidebar-item" onclick = "filterData2007()"> ${item} </div>`)
+	
+});
+
+function filterData2007() {
+		path = "data/plastic_pollution_2007.csv";
+		readCSV(path);
+}
+
+
+// 2008 
+time2008.forEach(function(item){
+	$('.sidebar').append(`<div class = "sidebar-item" onclick = "filterData(${item})"> ${item} </div>`)
+});
+	
+
+	function filterData(item) {
+
+	if (item = 2008 ) {
+		path = "data/plastic_pollution_2008.csv";
+		readCSV(path);
+	}
+}
+
+// 2009
+time2009.forEach(function(item){
+	$('.sidebar').append(`<div class = "sidebar-item" onclick = "filterData2009()"> ${item} </div>`)
+	
+});
+
+function filterData2009() {
+		path = "data/plastic_pollution_2009.csv";
+		readCSV(path);
+}
+
+// 2010
+time2010.forEach(function(item){
+	$('.sidebar').append(`<div class = "sidebar-item" onclick = "filterData2010()"> ${item} </div>`)
+	
+});
+
+function filterData2010() {
+		path = "data/plastic_pollution_2010.csv";
+		readCSV(path);
+}
+
+// 2011
+time2011.forEach(function(item){
+	$('.sidebar').append(`<div class = "sidebar-item" onclick = "filterData2011()"> ${item} </div>`)
+	
+});
+
+function filterData2011() {
+		path = "data/plastic_pollution_2011.csv";
+		readCSV(path);
+}
+
+
+// 2012
+time2012.forEach(function(item){
+	$('.sidebar').append(`<div class = "sidebar-item" onclick = "filterData2012()"> ${item} </div>`)
+	
+});
+
+function filterData2012() {
+		path = "data/plastic_pollution_2012.csv";
+		readCSV(path);
+}
+
+
+
+// 2013
+time2013.forEach(function(item){
+	$('.sidebar').append(`<div class = "sidebar-item" onclick = "filterData2013()"> ${item} </div>`)
+	
+});
+
+function filterData2013() {
+		path = "data/plastic_pollution.csv";
+		readCSV(path);
+}
+
+
+// old code
 function mapCSV(data){
 
 	// circle options
@@ -60,9 +159,6 @@ function mapCSV(data){
 	data.data.forEach(function(item,index){
 		// create a marker
 		let marker = L.circleMarker([item.latitude,item.longitude],circleOptions)
-		.on('mouseover',function(){
-			this.bindPopup(`${item.title}<br><img src="${item.thumbnail_url}">`).openPopup()
-		})
 
 		// add marker to featuregroup
 		markers.addLayer(marker)
